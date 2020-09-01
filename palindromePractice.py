0e121d6892215b6110e460337f6fd7730001b0b7
Daniel Litt <Daniel_litt@comcast.net>
2020-08-31 20:34:33 -0400

    Add alternate isPlalindrome function

--- palindromePractice.py
+++ palindromePractice.py
@@ -6,6 +6,9 @@ def isPalindrome(word):
         reversedWord = letter + reversedWord
     return reversedWord == word
 
+def isPalindromeAlt(word):
+    return word[::-1] == word
+
 class TestPalindrome(unittest.TestCase):
 
     def test_isPalindrome_0(self):
@@ -16,6 +19,15 @@ class TestPalindrome(unittest.TestCase):
     
     def test_isPalindrome_2(self):
         self.assertTrue(isPalindrome("1232112321"))
+
+    def test_isPalindrome_3(self):
+        self.assertTrue(isPalindromeAlt("racecar"))
+
+    def test_isPalindrome_4(self):
+        self.assertFalse(isPalindromeAlt("stuff"))
+    
+    def test_isPalindrome_6(self):
+        self.assertTrue(isPalindromeAlt("1232112321"))
 
 
 if __name__ == '__main__':
