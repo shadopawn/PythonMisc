f7b4e3226a3a23f95e01095343df15789e59b37f
Daniel Litt <daniel_litt@comcast.net>
2021-05-28 11:05:45 -0400

    Convert function to accept target sums

--- ZeroSumSubarrays.py
+++ ZeroSumSubarrays.py
@@ -1,7 +1,7 @@
 import unittest
 from collections import defaultdict
 
-def numberOfZeroSumSubarrays(numbers):
+def numberOfTargetSumSubarrays(numbers, targetSum = 0):
     previousSums = defaultdict(lambda: 0)
 
     zeroSumCount = 0
@@ -9,22 +9,24 @@ def numberOfZeroSumSubarrays(numbers):
     currentSum = 0
     for number in numbers:
         currentSum += number
-        if currentSum == 0:
+        if currentSum == targetSum:
             zeroSumCount += 1
         
-        if currentSum in previousSums:
-            zeroSumCount += previousSums[currentSum]
+        if (currentSum - targetSum) in previousSums:
+            zeroSumCount += previousSums[currentSum - targetSum]
         previousSums[currentSum] += 1
     
+    print(previousSums)
     return zeroSumCount
 
 
 class TestZeroSumSubarrays(unittest.TestCase):
 
     def test_find_number_of_subarrays_with_zero_sum(self):
-        self.assertEqual(numberOfZeroSumSubarrays([1, 2, 3]), 0)
-        self.assertEqual(numberOfZeroSumSubarrays([1, -1]), 1)
-        self.assertEqual(numberOfZeroSumSubarrays([2, -2, 3, 0, 4, -7]), 4)
+        self.assertEqual(numberOfTargetSumSubarrays([1, 2, 3]), 0)
+        self.assertEqual(numberOfTargetSumSubarrays([1, -1]), 1)
+        self.assertEqual(numberOfTargetSumSubarrays([1, -1, 1, -1]), 4)
+        self.assertEqual(numberOfTargetSumSubarrays([2, -2, 3, 0, 4, -7]), 4)
 
 
 if __name__ == '__main__':
